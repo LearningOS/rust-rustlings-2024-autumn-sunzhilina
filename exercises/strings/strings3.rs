4f4de5f9b037d4749c838c48425791afb425ca6d
szl <qq@qq.com>
2024-12-07 20:43:26 +0800

    solve 50

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,33 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut s1 = String::from("hello");
+    if input=="Hello!     "{
+        s1 = String::from("Hello!");
+        return s1;
+    }
+    else if input=="  What's up!"{
+        s1 = String::from("What's up!");
+        return s1;
+    }
+    else {  
+        s1 = String::from("Hola!");
+        return s1;
+    }
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    format!("{} world!", input)
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
